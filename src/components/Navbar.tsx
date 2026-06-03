7732853fdd4e436094a431941fcbde475f672194
AtheoJester23 <atheojester@gmail.com>
2026-06-03 23:24:20 +0800

    style: add note

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
+import {Link} from "react-router-dom"
+
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 w-full bg-nav py-5 px-10 shadow-md">
       <div>
-        <h1 className="text-white font-bold text-3xl">Online File Converter</h1>
+        <Link to="/" className="text-white font-bold text-3xl">Online File Converter</Link>
       </div>
     </nav>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,11 @@ import OptionCard from "./OptionCard"
 const Home = () => {
   return (
     <main className="bg-main h-[100vh] mt-19 gap-0">        
+      <section className="p-5 bg-blue-500 px-20 shadow border border-x-0 border-3 border-blue-600">
+        <h1 className="font-bold text-3xl text-white">Free converter</h1>
+        <p className="text-white">Convert files to different formats easily and for free.</p>
+      </section>
+      
       <section className="pt-20 flex grid place-items-center grid-cols-3">
         <OptionCard title="Video Converter" message="Convert videos to different formats" page="/vidConverter" Icon={FilmIcon}/>
         <OptionCard title="Audio Converter" message="Convert audio files to different formats" page="/audioConverter" Icon={MusicIcon}/>
